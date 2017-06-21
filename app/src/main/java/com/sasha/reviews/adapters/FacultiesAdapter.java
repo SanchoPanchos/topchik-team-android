59fa1e83933dbecd653ce992ba3f5e53fd3db85d
SanchPancho <SanchoPancho5695@gmail.com>
2017-06-21 18:06:00 -0400

    Box version probably

--- app/src/main/java/com/sasha/reviews/adapters/FacultiesAdapter.java
+++ app/src/main/java/com/sasha/reviews/adapters/FacultiesAdapter.java
@@ -1,42 +1,10 @@
 package com.sasha.reviews.adapters;
 
-import android.app.Activity;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
-import com.bumptech.glide.Glide;
-import com.sasha.reviews.R;
-import com.sasha.reviews.model.Faculty;
+/**
+ * Created by Sasha on 21.06.2017.
+ */
 
-import java.util.List;
-
-public class FacultiesAdapter extends RecyclerView.Adapter<FacultiesViewHolder> {
-
-    private Activity context;
-    private List<Faculty> faculties;
-
-    public FacultiesAdapter(Activity context, List<Faculty> faculties) {
-        this.context = context;
-        this.faculties = faculties;
-    }
-
-    @Override
-    public FacultiesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.card_faculty, parent, false);
-        return new FacultiesViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(FacultiesViewHolder holder, int position) {
-        holder.text.setText(faculties.get(position).getNameShort());
-        Glide.with(context).load(faculties.get(position).getImg()).into(holder.image);
-    }
-
-    @Override
-    public int getItemCount() {
-        return faculties.size();
-    }
+public class FacultiesAdapter extends BaseAdapter {
 }
